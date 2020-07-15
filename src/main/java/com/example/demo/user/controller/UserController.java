9f40edde51261105dc73eef8fed32684d9d2effb
holidenty <gldiwkd@ajou.ac.kr>
2020-07-15 16:41:49 +0900

    first commit

---
+++ src/main/java/com/example/demo/user/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.demo.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user-api")
+
+public class UserController {
+
+}
